0f97f064dc19e14fa1c2fe6463891a924b93cf2f
moayuisuda <874706277@qq.com>
2025-01-27 17:34:44 +0800

    WIP

--- src/Roll/Store.ts
+++ src/Roll/Store.ts
@@ -200,7 +200,7 @@ export class RollStore implements RollState {
 
   @action
   play = () => {
-    this.stop(false);
+    if (this.status === "playing") this.stop(false);
     // if (this.status === "playing") return;
     this.start();
 
